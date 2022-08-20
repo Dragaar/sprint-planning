0477490fa13f4da1086af0d60a0cd3f5f103d216
Dragaar <rosivanyshyn@gmail.com>
2022-08-20 19:58:16 +0300

    111

--- src/main/java/com/epam/rd/autotasks/sprintplanning/tickets/UserStory.java
+++ src/main/java/com/epam/rd/autotasks/sprintplanning/tickets/UserStory.java
@@ -1,23 +1,30 @@
 package com.epam.rd.autotasks.sprintplanning.tickets;
 
 public class UserStory extends Ticket {
+    private UserStory[] dependsOn;
 
     public UserStory(int id, String name, int estimate, UserStory... dependsOn) {
         super(id, name, estimate);
-        throw new UnsupportedOperationException("Implement this method");
+
+        this.dependsOn = dependsOn;
     }
 
     @Override
     public void complete() {
-        throw new UnsupportedOperationException("Implement this method");
+        for(UserStory e: dependsOn)
+        {
+            if(e.isCompleted()==false) return;
+        }
+        super.complete();
     }
 
     public UserStory[] getDependencies() {
-        throw new UnsupportedOperationException("Implement this method");
+        return dependsOn.clone(); //клонує об'єкт і надає доступ лишень до його копії
     }
 
+    //[US 1] User Story 1
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Implement this method");
+       return "[US "+getId()+"] "+getName();
     }
 }
